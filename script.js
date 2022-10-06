076309e9f90fcb1603d4427480f48b7609cde3d8
Bartek0074 <bartekdo123@gmail.com>
2022-10-06 23:28:45 +0200

    added swap function

--- script.js
+++ script.js
@@ -2,25 +2,33 @@ const currencyOne = document.querySelector('#currency-one');
 const amountOne = document.querySelector('.amount-one');
 const currencyTwo = document.querySelector('#currency-two');
 const amountTwo = document.querySelector('.amount-two');
-const swap = document.querySelector('.swap');
+const swapBtn = document.querySelector('.swap');
 const rateInfo = document.querySelector('.rate-info');
 
 const calculate = () => {
     fetch(`https://api.exchangerate.host/convert?from=${currencyOne.value}&to=${currencyTwo.value}`)
     .then(res => res.json())
     .then(data => {
-        const currency1 = currencyOne.value;
-        const currency2 = currencyTwo.value;
         rateInfo.textContent = `1 ${currencyOne.value} = ${data.result.toFixed(4)} ${currencyTwo.value}`
         
         amountTwo.value = (amountOne.value * data.result).toFixed(2);
     })
 }
 
-calculate();
+const swap = () => {
+    const oldCurrency = currencyOne.value;
+
+    currencyOne.value = currencyTwo.value;
+    currencyTwo.value = oldCurrency;
+
+    amountOne.value = amountTwo.value;
+
+    calculate();
+}
 
 amountOne.addEventListener('keyup', calculate);
 currencyOne.addEventListener('change', calculate);
 currencyTwo.addEventListener('change', calculate);
+swapBtn.addEventListener('click', swap)
 
 calculate();
